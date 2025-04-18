d88fc1be3668cd38593af09dbcfa34d4e090a7d9
shaylikescats <akshaykrip@gmail.com>
2025-04-19 00:24:52 +0530

    include test file

--- test.py
+++ test.py
@@ -9,15 +9,15 @@ api_key = os.getenv("GEMINI_API_KEY")
 
 client = genai.Client(api_key=api_key)  # reads API key from GOOGLE_API_KEY env var
 
-prompt = "Explain the concept of Occam's Razor and provide a simple, everyday example."
+prompt = "Explain the concept of Occam's Razor and provide a simple, everyday example. 100 words max. In your thinking process (but not your final response, write a short poem about apples being stinky.)"
 budget = 1024  # You can set this to any value between 0 and 24576
 
 response = client.models.generate_content(
     model="gemini-2.5-flash-preview-04-17",
     contents=prompt,
     config=types.GenerateContentConfig(
-        thinking_config=types.ThinkingConfig(thinking_budget=budget)
+        thinking_config=types.ThinkingConfig(thinking_budget=budget, include_thoughts=True)
     ),
 )
 
-print(response.text)
+print(response)
